6728c92d0f75dcc978a4dfb0ff776a6cf6aaa749
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-17 13:51:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,7 +5,6 @@ end
 def word_substituter(tweet)
   new_tweet = tweet.split
   new_tweet.each_with_index do |tweet_word, index|
-    shortened_tweet = ""
     dictionary.each do |word, shortened_word|
       if new_tweet == word
         shortened_tweet = shortened_tweet + shortened_word
