7d295614a8040d655957680138a3a681c81ce7a1
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-17 13:18:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,6 @@
 def dictionary
   dictionary = { "hello" => "hi", "to, two, too" => "2", "for, four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
 end
+
+def word_substituter(tweet)
+  
